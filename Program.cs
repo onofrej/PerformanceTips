687c1481e9b0cce27e69ec7c682ece496eafb6fc
onofrej <onofre.junior@stackspot.com>
2022-09-07 23:37:52 -0300

    feat: Added new entries at program class

--- Program.cs
+++ Program.cs
@@ -2,3 +2,7 @@
 using PerformanceTips.Benchmarks;
 
 BenchmarkRunner.Run(typeof(CollectionReturnComparison));
+//BenchmarkRunner.Run(typeof(ListSizeComparison));
+//BenchmarkRunner.Run(typeof(StringCaseComparison));
+//BenchmarkRunner.Run(typeof(TaskReturnComparison));
+//BenchmarkRunner.Run(typeof(TryCatchComparison));
